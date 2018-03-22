064a7987202345f0ceef029c4debcf14b7d252e7
luiscajl <luisca_jl@hotmail.com>
2018-03-22 11:17:16 +0100

    Quit languaje support its need for universal. Routing is working

--- src/app/layout/components/sidebar/sidebar.component.ts
+++ src/app/layout/components/sidebar/sidebar.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-sidebar',
@@ -12,12 +11,8 @@ export class SidebarComponent {
     showMenu: string = '';
     pushRightClass: string = 'push-right';
 
-    constructor(private translate: TranslateService, public router: Router) {
-        this.translate.addLangs(['en', 'fr', 'ur', 'es', 'it', 'fa', 'de']);
-        this.translate.setDefaultLang('en');
-        const browserLang = this.translate.getBrowserLang();
-        this.translate.use(browserLang.match(/en|fr|ur|es|it|fa|de/) ? browserLang : 'en');
-
+    constructor( public router: Router) {
+       
         this.router.events.subscribe(val => {
             if (
                 val instanceof NavigationEnd &&
@@ -56,9 +51,6 @@ export class SidebarComponent {
         dom.classList.toggle('rtl');
     }
 
-    changeLang(language: string) {
-        this.translate.use(language);
-    }
 
     onLoggedout() {
         localStorage.removeItem('isLoggedin');
