057724361c6fdaaa13dabab8f79d2a3d067716be
luiscajl <luisca_jl@hotmail.com>
2018-03-28 15:38:19 +0200

    Change

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,2 +1,2 @@
-export const BASEURL:string = 'https://api.videotranscoding.es';
+export const BASEURL:string = 'https://api.videotranscoding.es/';
 export const USER_BASEURL: string = BASEURL + 'user/';
